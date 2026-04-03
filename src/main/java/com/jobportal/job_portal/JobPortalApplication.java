54744eeafa0108016a85580f87d0217f11c6ee86
“[Prachi <[rprachi518@gmail.com]>
2026-04-03 17:07:20 +0530

    exclude UserDetailsService via annotation

--- src/main/java/com/jobportal/job_portal/JobPortalApplication.java
+++ src/main/java/com/jobportal/job_portal/JobPortalApplication.java
@@ -2,8 +2,12 @@ package com.jobportal.job_portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.jobportal.job_portal")
+@SpringBootApplication(
+		scanBasePackages = "com.jobportal.job_portal",
+		exclude = {UserDetailsServiceAutoConfiguration.class}
+)
 public class JobPortalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobPortalApplication.class, args);
