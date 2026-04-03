79464f40647dca48875ca18f2ca73858df6c3cc6
“[Prachi <[rprachi518@gmail.com]>
2026-04-03 15:30:30 +0530

    fix component scan

--- src/main/java/com/jobportal/job_portal/JobPortalApplication.java
+++ src/main/java/com/jobportal/job_portal/JobPortalApplication.java
@@ -3,11 +3,9 @@ package com.jobportal.job_portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.jobportal.job_portal")
 public class JobPortalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobPortalApplication.class, args);
 	}
-
 }
